07d3b0dccfecba8ef7a70bec9397073f64d63900
deding2000 <chr2830@gmail.com>
2023-03-10 12:07:59 +0100

    added plots
    and also added stuff in simulerings fil

--- Assignment_2/Ex21.r
+++ Assignment_2/Ex21.r
@@ -2,7 +2,7 @@
 p <- 2 # AR order
 q <- 3 # MA order
 
-theta <- c(-0.8) #skal måske være plus
+theta <- c(0.8) #skal måske være plus
 phi <- c(0.8,-0.5)
 
 cat("AR parameters:",round(theta,3))
@@ -18,7 +18,7 @@ for (i in 1:10) {
     y[i,] <- ts[1:200]
     t = c(1:200)
     if (i == 1) {
-    plot(t,y[i,],type="l",col=toString(colours[i]),xlab="t",ylab="X_t")
+    plot(t,y[i,],type="l",col=toString(colours[i]),xlab="t",ylab=expression(x[t]),main ="Realization of 10 ARMA(1,2) processes")
     }
     else {
     lines(t,y[i,],type="l",col=toString(colours[i]))  
@@ -29,7 +29,7 @@ for (i in 1:10) {
 for (i in 1:10) {
     ACF <- acf(y[i,],main="ACF",plot = FALSE) 
     if (i == 1) {
-    plot(ACF$lag,ACF$acf,type="h",col=toString(colours[i]),xlab="lag",ylab="ACF")
+    plot(ACF$lag,ACF$acf,type="h",col=toString(colours[i]),xlab="Lag",ylab="ACF",main="ACF for 10 ARMA(1,2) processes")
     #lines(ACF$acf, ci.type="white",col=toString(colours[i]))
     }
     else {
@@ -38,16 +38,25 @@ for (i in 1:10) {
     }
 
 }
+
 #confidence intervals (assuming white noise):
 ci = 0.95
 abline(h=qnorm((1 + ci)/2)/sqrt(n),lty=2)
 abline(h=-qnorm((1 + ci)/2)/sqrt(n),lty=2)
 abline(h = 0)
 
+#theoritical ACF
+fun_acf <- function(k) {
+    if (k == 1) {
+        return(1)
+    }
+}
+
+
 for (i in 1:10) {
     PACF <- pacf(y[i,],main="ACF",plot = FALSE) 
     if (i == 1) {
-    plot(PACF$lag,PACF$acf,type="h",col=toString(colours[i]),xlab="lag",ylab="PACF")
+    plot(PACF$lag,PACF$acf,type="h",col=toString(colours[i]),xlab="Lag",ylab="PACF",main="PACF for 10 ARMA(1,2) processes")
     }
     else {
     lines(PACF$lag,PACF$acf,type="h",col=toString(colours[i]))  
@@ -57,3 +66,9 @@ for (i in 1:10) {
 abline(h=qnorm((1 + ci)/2)/sqrt(n),lty=2)
 abline(h=-qnorm((1 + ci)/2)/sqrt(n),lty=2)
 abline(h = 0)
+
+#Variance of each realization
+vars = rep(0,10)
+for (i in 1:10) {
+    vars[i] <- var(y[i,])
+}
