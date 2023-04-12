19e4b64ef15336d6b7ca658cc9c14da0d976dfd8
deding2000 <chr2830@gmail.com>
2023-04-12 16:59:55 +0200

    Marima forecasting works!

--- Assignment_3/ass3_Q2_deding.r
+++ Assignment_3/ass3_Q2_deding.r
@@ -27,46 +27,60 @@ pacf(Mitjut)
 pacf(Sealand)
 
 ## Marima time
-rural <- define.dif(log(Rural),difference=c(1,1))$dif.series
-mitjut <- define.dif(log(Mitjut),difference=c(1,1))$dif.series
-cph <- define.dif(log(CPH),difference=c(1,1))$dif.series
-sealand <- define.dif(log(Sealand),difference=c(1,1))$dif.series
-interest <- define.dif(sqrt(Interest),difference=c(1,1))$dif.series
-inflation <- Inflation
-orgdata <- data.frame(t(log(CPH)),t(log(Sealand)),t(log(Mitjut)),t(log(Rural)),t(Interest[1:122]),Inflation[1:122])
-mdata <- data.frame(t(cph),t(sealand),t(mitjut),t(rural),interest[1:122],inflation[1:122])
+######
+#GAMMEL FREMGANGSMÅDE
+# Transforming data
+# rural <- define.dif(log(Rural),difference=c(1,1))$dif.series
+# mitjut <- define.dif(log(Mitjut),difference=c(1,1))$dif.series
+# cph <- define.dif(log(CPH),difference=c(1,1))$dif.series
+# sealand <- define.dif(log(Sealand),difference=c(1,1))$dif.series
+# interest <- define.dif(sqrt(Interest),difference=c(1,1))$dif.series
+# inflation <- Inflation
+# Creating data frames. First one is to estimat MARIMA model and second one is for forecasting
+# mdata <- data.frame(t(cph),t(sealand),t(mitjut),t(rural),interest[1:122],inflation[1:122])
+# orgdata <- data.frame(t(log(CPH)),t(log(Sealand)),t(log(Mitjut)),t(log(Rural)),t(Interest[1:122]),Inflation[1:122])
 
-# estimated from acf and pacf plots
-ar <- c(2)
-ma <- c(4)
-Model1 <- define.model(kvar=6, ar=ar, ma=ma, indep=c(6),rem.var=c(5))
-#Model1 <- define.model(kvar=6, ar=ar, ma=ma, indep=c(6),rem.var=c(5),ar.rem=c(2,1,2,2,4,2,3,3,2),ma.rem=c(3,2,4,3,3,4,1,6,4))#no.dep=c(1,3,2,2,2,4))#no.dep=c(1,2,1,3,1,4))
-Marima1 <- marima(mdata,means=1,
+
+####
+#NY FREMGANGSMÅDE (VIRKER)
+Data_log <- Data
+Data_log$Capital <- log(Data_log$Capital)
+Data_log$Sealand<- log(Data_log$Sealand)
+Data_log$MidJutland <- log(Data_log$MidJutland )
+Data_log$Rural <- log(Data_log$Rural)
+
+# Using differenced data
+Data_log_dif <- define.dif(series = ts(na.omit(Data_log)),difference=c(3,1,4,1,5,1,6,1,7,1))
+poly <- Data_log_dif$dif.poly
+data.dif <- Data_log_dif$y.dif
+
+# Estimated from acf and pacf plots
+ar <- c(1,2)
+ma <- c(2)
+Model1 <- define.model(kvar=8, ar=ar, ma=ma,indep=c(8),rem.var=c(1,2,7))
+Marima1 <- marima(data.dif,means=1,
                   ar.pattern=Model1$ar.pattern, ma.pattern=Model1$ma.pattern,
-                  Check=FALSE, Plot="none", penalty=2)
+                  Check=FALSE, Plot="none", penalty=1)
 
 short.form(Marima1$ar.estimates, leading=FALSE) # print estimates
 short.form(Marima1$ma.estimates, leading=FALSE)
 Marima1$ar.pvalues
 Marima1$ma.pvalues
 Marima1$ar.stdv
+# Look at time series number x
+# order is (quarter(1),DK(2),CPH(3),Sealand(4),Mid(5),Rural(6),Interest(7),Inflation(8))
+x <- 5
 
-
-# kig på variabel x
-x <- 3
+#Residuals
 res <- na.omit(Marima1$residuals[x,])
 residual_plots(res)
 residual_sign_test(res)
 
-
-fit <- (Marima1$fitted[x,])
-av <- Marima1$averages[x]
-mean(diff(log(Mitjut)))
+# Fitting and predicting
+MARIMA_forecast <- arma.forecast(series=ts(Data_log), marima =Marima1,nstart=122,nstep=6,dif.poly = poly)
 par(mfrow=c(2,1))
-plot(Mitjut)
-fit_summed <- define.sum(t(na.omit(fit)),difference=c(1,1))$series.sum
-plot(exp(fit_summed[1:122]))
+plot(MARIMA_forecast$forecasts[x,])
+plot(log(Mitjut))
 
-Marima1$one.step
-forecasting <- arma.forecast(series=log(Data[,3:8]), marima =Marima1,nstart=122,nstep=6)
-plot(exp(forecasting$forecasts[x,]))
+
+
