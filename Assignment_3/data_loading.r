b98743a5cdfadf51d383fc9867978c7ac1274dbc
deding2000 <chr2830@gmail.com>
2023-04-13 13:57:20 +0200

    marima preds plots

--- Assignment_3/data_loading.r
+++ Assignment_3/data_loading.r
@@ -2,7 +2,7 @@ Data <- read.csv("Time_Series_2023/Assignment_3/A3Data.csv",header=TRUE)
 N_prices <- 122
 N_inf <- 124
 N_int <- 124
-Years <- seq(from =1992.50,to = 2024, by = 0.25)
+Years <- seq(from =1992.25,to = 2024, by = 0.25)
 pricesDK <- na.omit(Data$Denmark)
 CPH <- na.omit(Data$Capital)
 Rural <- na.omit(Data$Rural)
