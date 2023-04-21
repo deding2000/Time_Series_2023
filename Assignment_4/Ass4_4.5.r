e1f8257455de677eedf8aecfb78ba3b3eae54e14
OscarBOPedersen <123966957+OscarBOPedersen@users.noreply.github.com>
2023-04-21 12:19:17 +0200

    Update 4.5

--- Assignment_4/Ass4_4.5.r
+++ Assignment_4/Ass4_4.5.r
@@ -21,39 +21,49 @@ C <- matrix(c(1))
 Sigma1 <- matrix(c(0.01)) #System variance
 Sigma2 <- matrix(c(0.005)) #Observation variance
 
-#Question 4.3 Pure filter.
+#ML estimate
+Sigma <- {Sigma1,Sigma2}
+Sigma
+kl <- kalman(Sal,A=A,B=B,u=NULL,C=C,Sigma.1=matrix(Sigma[1]),Sigma.2=matrix(Sigma[2]),debug=FALSE,V0=matrix(Sigma[1]),Xhat0=Sal[1],n.ahead=1,skip=0,verbose=TRUE)
+#using C is single index then
+print(kl$Outliers)
+N <- length(Sal)
+R <- na.omit( (c(C)*kl$Sigma.xx.pred[1,1,]*c(C)+c(matrix(Sigma[2])))[1:N]+0*(Sal[1:N]-kl$pred[1:N]) )
+Y_tilde <- na.omit(Sal[1:N]-kl$pred[1:N])
+
+-1/2*sum(log(R+Y_tilde*1/R*Y_tilde))
+
+#hvad gør man med NA values her så?????
+
 source("kalman.R")
-kalman43 <- kalman(Sal,A=A,B=B,u=NULL,C=C,Sigma.1=Sigma1,Sigma.2=Sigma2,debug=FALSE,V0=Sigma1,Xhat0=Sal[1],n.ahead=1,skip=0,verbose=TRUE)
+nll <- function(Sigma){ #Sigma[1] og Sigma[2].  Sigma <- array(c(Sigma1,Sigma2),dim=c(1,1,2))
+  kl <- kalman_rem_out(Sal,A=A,B=B,u=NULL,C=C,Sigma.1=matrix(Sigma[1]),Sigma.2=matrix(Sigma[2]),debug=FALSE,V0=matrix(Sigma[1]),Xhat0=Sal[1],n.ahead=1,skip=0,verbose=TRUE)
+  #using C is single index then
+  kl$Outliers
+  N <- length(Sal)
+  R <- na.omit( (c(C)*kl$Sigma.xx.pred[1,1,]*c(C)+c(matrix(Sigma[2])))[1:N]+0*(Sal[1:N]-kl$pred[1:N]) )
+  Y_tilde <- na.omit(Sal[1:N]-kl$pred[1:N])
+  
+  1/2*sum(log(R)+Y_tilde*1/R*Y_tilde)
+}
+fit <- optim(c(Sigma1,Sigma2), nll,method="L-BFGS-B" , lower = 0.00005) #indsæt lower bound. #method="L-BFGS-B"
+fit$convergence
+fit
 
-#1. Plot prediction along the data include 95% PI.
-plot(Days,Sal,type="p",,col="red",xlab="Time (day)",ylab="Water Salinity [PSU] = [g/kg]")
-matlines(Days,kalman43$pred[1:5000,1]+sqrt(kalman43$Sigma.yy.pred[1,1,1:5000])%*%cbind(0,-1.96,1.96),col="seagreen",lty=c(1,2,2), lwd=1)
-legend("bottomright",legend=c("Observations","1-step prediciions"),col=c("tomato","seagreen"),pch=c(20,NA),bty='n',lty=c(NA,1))
 
-#2. Plot the standardized one step prediction errors. (Prediction errors normalized with the standard error of the prediction.)
-length(kalman43$pred)
 
-errors_norm <- na.omit((kalman43$pred[1:5000,1]-Sal[1:5000]) / sqrt(kalman43$Sigma.yy.pred[1:5000]) )
-plot(errors_norm,ylab="Errors normalized",type='l')
+source("kalman_rem_out.R")
+nll <- function(Sigma){ #Sigma[1] og Sigma[2].  Sigma <- array(c(Sigma1,Sigma2),dim=c(1,1,2))
+  kl <- kalman_rem_out(Sal,A=A,B=B,u=NULL,C=C,Sigma.1=matrix(Sigma[1]),Sigma.2=matrix(Sigma[2]),debug=FALSE,V0=matrix(Sigma[1]),Xhat0=Sal[1],n.ahead=1,skip=0,verbose=TRUE)
+  #using C is single index then
+  
+  N <- length(Sal)
+  R <- na.omit( (c(C)*kl$Sigma.xx.pred[1,1,]*c(C)+c(matrix(Sigma[2])))[1:N]+0*(Sal[1:N]-kl$pred[1:N]) )
+  Y_tilde <- na.omit(Sal[1:N]-kl$pred[1:N])
+  1/2*sum(log(R)+Y_tilde*1/R*Y_tilde)
+}
+fit <- optim(c(Sigma1,Sigma2), nll,method="L-BFGS-B" , lower = 0.00005) #indsæt lower bound. #method="L-BFGS-B"
+fit$convergence
+fit
 
-interval = c(800:950)
-#3. Plot for 800-950
-plot(interval,Sal[interval],type="p",,col="red",xlab="Index",ylab="Water Salinity [PSU] = [g/kg]",ylim=c(15,24))
-matlines(interval,kalman43$pred[interval,1]+sqrt(kalman43$Sigma.yy.pred[1,1,interval])%*%cbind(0,-1.96,1.96),col="seagreen",lty=c(1,2,2), lwd=1)
-legend("bottomright",legend=c("Observations","1-step prediciions"),col=c("tomato","seagreen"),pch=c(20,NA),bty='n',lty=c(NA,1))
-
-errors_norm <- na.omit((kalman43$pred[interval,1]-Sal[interval]) / sqrt(kalman43$Sigma.yy.pred[interval]) )
-#MAKE PLOT!!!
-plot(interval,errors_norm,ylab="Errors normalized",type='l',xlab="Index")
-
-#MAKE TABLE!!
-maketable <- data.frame(Sal = Sal[5000], rec = kalman43$rec[5000],
-pred = kalman43$pred[5000],
-K = kalman43$K[5000],
-Sigma.xx.rec = kalman43$Sigma.xx.rec[1,1,5000],
-Sigma.yy.rec=kalman43$Sigma.yy.rec[5000],
-kalman43$Sigma.xx.pred[5000],
-kalman43$Sigma.yy.pred[5000])
-
-maketable
 
